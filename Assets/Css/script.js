64e912e2e7fc976927d449ccc03a50afe32525b0
Devstmike <139519652+Devstmike@users.noreply.github.com>
2024-02-14 00:12:12 +0100

    Update script.js

--- Assets/Css/script.js
+++ Assets/Css/script.js
@@ -33,3 +33,57 @@ cancelIcon.onclick = () => {
     MenuIcon.classList.remove("hideMe")
     cancelIcon.classList.remove("showMe");
 }
+
+
+
+function preventFormSubmit() {
+    var forms = document.querySelectorAll('form');
+    //alert("prevented")
+    for (var i = 0; i < forms.length; i++) {
+    forms[i].addEventListener('submit', function(event) {
+    event.preventDefault();
+    SendData()
+    });
+    }
+}
+
+ window.addEventListener("load", preventFormSubmit); 
+
+// I used Google Apps Script to handle Email
+function SendData() {
+    // e.preventDefault()
+     const formEle = document.querySelector(".hero-Contact-Page form");
+     const formDatab = new FormData(formEle);
+     console.log("sent")
+     console.log(formDatab)
+     formDatab.forEach(el => {
+         console.log(el)
+     })
+     fetch(
+     "https://script.google.com/macros/s/AKfycbwcGmdLuLK81k02EvRmvsexi8iiah0Cz-qmV7-JyzCxMjJR9CVDEkzMVx2jCmDX2SjEEw/exec",
+     {
+         method: "POST",
+         body: formDatab
+     }
+     )
+     .then((res) => res.text())
+     .then((data) => {
+         console.log(data);
+     })
+     .catch((error) => {
+         console.log(error);
+     });
+
+    formEle.reset()
+
+    let thankyou =  `<div class="thanks" style="text-align:center">
+         <p style="Color:green">
+             Thank You!
+         </p>
+         <p>
+             Your Message has been Sent.
+        </p>
+     </div>`;
+     document.querySelector("form").innerHTML = thankyou
+ }
+ 
